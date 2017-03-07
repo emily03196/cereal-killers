c5e2bda33e47199cc86a1a3a83fb7bc510ffaa10
Anne Rogers <amr@cs.uchicago.edu>
2017-03-07 01:21:24 -0600

    finished counter

--- reviews.py
+++ reviews.py
@@ -3,19 +3,27 @@ import string
 import sys
 from collections import Counter
 
+REMOVE_WORDS = ['the', 'a', 'an', 'but', 'if', 'so', 'they', 'them', 'is', 'and', 'to', 'of',\
+ 'for', 'it', '', 'very', 'their', 'this', 'i', 'my', 'are']
 
-def clean_up_review(review):
+def clean_up_review(reviews):
     '''
-    Review string
+    List of review strings
     '''
-    remove_punctuation = re.compile('[%s]' % re.escape(string.punctuation))
-    review = remove_punctuation.sub('', review)
-    review_list = review.split(' ')
+    for review in reviews:
+        remove_punctuation = re.compile('[%s\d]' % re.escape(string.punctuation))
+        review = remove_punctuation.sub('', review).lower()
+        review_list = review.split(' ')
+        review_list = [word for word in review_list if word not in REMOVE_WORDS]
 
-    review_words_count = Counter(review_list)
-    top_words = review_words_count.most_common(3)
+        review_words_count = Counter(review_list)
+        words = review_words_count.most_common(5)
+        top_words = [words[i] for i in range(len(words))]
+        top_words = sorted(top_words, key=lambda x: x[1], reverse = True)
 
-    review_pairs_count = Counter(zip(review_list, review_list[1:]))
-    top_pairs = review_pairs_count.most_common(3)
+        review_pairs_count = Counter(zip(review_list, review_list[1:]))
+        pairs = review_pairs_count.most_common(20)
+        top_pairs = [pairs[i] for i in range(len(pairs))]
+        top_pairs = sorted(top_pairs, key=lambda x: x[1], reverse = True)
 
-# hardcode not important words
+    return top_words, top_pairs
